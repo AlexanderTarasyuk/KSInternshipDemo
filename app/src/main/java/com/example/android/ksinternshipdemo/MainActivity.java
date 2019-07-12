f0dfdf3e845abaf781b59cad23b5e748f711b174
d.shevtsov <d.shevtsov@keepsolid.com>
2019-07-12 11:31:38 +0300

    step2: saveState implemented

--- app/src/main/java/com/example/android/ksinternshipdemo/MainActivity.java
+++ app/src/main/java/com/example/android/ksinternshipdemo/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatCheckBox;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -25,9 +26,19 @@ public class MainActivity extends AppCompatActivity {
     private boolean soulChecked;
     private boolean spamChecked;
 
+    private final static String KEY_EMAIL = "KEY_EMAIL";
+    private final static String KEY_PASSWORD = "KEY_PASSWORD";
+    private final static String KEY_SOUL_TRADE = "KEY_SOUL_TRADE";
+    private final static String KEY_SPAM_AGREED = "KEY_SPAM_AGREED";
+
+    private final static String LOG_TAG = "SaveStateActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d(LOG_TAG, "onCreate()");
+
         setContentView(R.layout.activity_main);
 
         toolbar = findViewById(R.id.toolbar);
@@ -53,6 +64,28 @@ public class MainActivity extends AppCompatActivity {
                 readSavedData();
             }
         });
+
+        checkForSavedState(savedInstanceState);
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(LOG_TAG, "onSaveInstanceState()");
+
+        outState.putString(KEY_EMAIL, emailText);
+        outState.putString(KEY_PASSWORD, passText);
+        outState.putBoolean(KEY_SOUL_TRADE, soulChecked);
+        outState.putBoolean(KEY_SPAM_AGREED, spamChecked);
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        // This One Called after "onStart()". "savedInstanceState" guaranteed not to be null.
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(LOG_TAG, "onRestoreInstanceState()");
 
     }
 
@@ -84,6 +117,47 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
 
+    }
+
+    private void checkForSavedState(Bundle stateBundle) {
+        if (stateBundle == null) {
+            return;
+        }
+
+        emailText = stateBundle.getString(KEY_EMAIL);
+        passText = stateBundle.getString(KEY_PASSWORD);
+        soulChecked = stateBundle.getBoolean(KEY_SOUL_TRADE);
+        spamChecked = stateBundle.getBoolean(KEY_SPAM_AGREED);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy()");
     }
 
 }
