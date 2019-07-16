7bd7f0d9f4dac5d41fc0807bd89b9e19ac8c4986
d.shevtsov <d.shevtsov@keepsolid.com>
2019-07-16 13:35:33 +0300

    step1: Basic listview with custom adapter

--- app/src/main/java/com/example/android/ksinternshipdemo/MainActivity.java
+++ app/src/main/java/com/example/android/ksinternshipdemo/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.android.ksinternshipdemo;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.example.android.ksinternshipdemo.adapters.TaskAdapter;
+import com.example.android.ksinternshipdemo.models.TaskItem;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+
+    private ListView tasksListView;
+    private TaskAdapter adapter;
+    private ArrayList<TaskItem> items;
 
 
     private final static String LOG_TAG = MainActivity.class.getSimpleName();
@@ -22,6 +35,41 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Image viewer");
+
+        tasksListView = (ListView) findViewById(R.id.list_view);
+
+        items = new ArrayList<>();
+
+        items.add(new TaskItem(true, "Создать этот список", TaskItem.Type.ALARM, "10:00", "07/05/2019"));
+        items.add(new TaskItem(true, "Отметить первый пункт как готовый", TaskItem.Type.ALARM, "11:25", "07/05/2019"));
+        items.add(new TaskItem(true, "Осознать что 2 пункта уже готовы", TaskItem.Type.NOTE, "11:30", "07/05/2019"));
+        items.add(new TaskItem(false, "Вздремнуть", TaskItem.Type.PLACE, "11:35", "07/05/2019"));
+        items.add(new TaskItem(false, "Заказать воду", TaskItem.Type.ALARM, "16:00", "09/05/2019"));
+        items.add(new TaskItem(true, "Заплатить за интернет", TaskItem.Type.NOTE, "10:00", "11/05/2019"));
+        items.add(new TaskItem(true, "Придумать следующий элемент списка", TaskItem.Type.NOTE, "12:00", "11/05/2019"));
+        items.add(new TaskItem(false, "Сходить на работу", TaskItem.Type.PLACE, "09:00", "12/05/2019"));
+        items.add(new TaskItem(false, "Провести занятие интернатуры", TaskItem.Type.PLACE, "13:00", "12/05/2019"));
+        items.add(new TaskItem(false, "Развидеть увиденное", TaskItem.Type.ALARM, "00:00", "13/05/2019"));
+        items.add(new TaskItem(false, "Перевернуть пингвина", TaskItem.Type.NOTE, "22:00", "13/05/2019"));
+        items.add(new TaskItem(true, "Залипнуть в инете", TaskItem.Type.NOTE, "12:00", "14/05/2019"));
+        items.add(new TaskItem(true, "Впихнуть невпихуемое", TaskItem.Type.PLACE, "13:00", "15/05/2019"));
+
+
+        adapter = new TaskAdapter(items, this);
+
+        tasksListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Full task name")
+                        .setMessage(adapter.getItems().get(position).getTaskName())
+                        .setCancelable(true)
+                        .create().show();
+            }
+        });
+
+        tasksListView.setAdapter(adapter);
+
 
     }
 
